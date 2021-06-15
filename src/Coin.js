8ad34a9ceecd1e8d202b7bc68de956ddf029f5c5
mkurmarc <51379562+mkurmarc@users.noreply.github.com>
2021-06-14 23:35:31 -0700

    working on dumb Coin component

---
+++ src/Coin.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Coin.css';
+
+class Coin extends Component {
+    render() { 
+        return (
+            <img src=''></img>
+        );
+    }
+}
+ 
+export default Coin;
