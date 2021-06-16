ba7df8458fb12b35caa7d81849a493d6d5b2edf9
mkurmarc <51379562+mkurmarc@users.noreply.github.com>
2021-06-16 08:25:49 -0700

    added CoinFlip comp, Coin comp complt, logic and button works

--- src/Coin.js
+++ src/Coin.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './Coin.css';
 
+
 class Coin extends Component {
     render() {
         return (
--- src/CoinFlip.js
+++ src/CoinFlip.js
@@ -3,40 +3,57 @@ import Coin from './Coin';
 import './Coin.css';
 
 class CoinFlip extends Component {
-    static defaultProps ={
+    static defaultProps = {
         title: "Let's Flip This Coin",
-        counter: 0,
         facesArr: ["heads", "tails"]
     };
 
     constructor(props) {
         super(props);
         this.state = {
-            
+            coin1: 'heads',
+            c1TailsCount: 0,
+            c1HeadsCount: 0,
+            c1TotalFlips: 0
         };
         this.handleClick = this.handleClick.bind(this);
     }
 
     flip() {
-        console.log(this.props.facesArr[Math.floor(Math.random() * 2)]);
-        
+        // stores randomized item from facesArr prop in const to set the state 
+        const newCoin1 = this.props.facesArr[
+            Math.floor(Math.random() * 2)
+        ];
+
+        if(newCoin1 === "heads") {
+            this.state.c1HeadsCount += 1; // adds to head count for coin 1
+        } else {
+            this.state.c1TailsCount += 1; // adds to tail count for coin 1
+        }
+
+        this.state.c1TotalFlips += 1; // adds to total flips for coin 1
+        this.setState({ coin1: newCoin1 });
     }
 
     handleClick() {
         this.flip();
     }
 
-    render() { 
+    render() {
         return (
             <section className='CoinFlip'>
                 <h1>{this.props.title}</h1>
-                <Coin face={'heads'} />
+                <Coin face={this.state.coin1} />
                 <div>
                     <button onClick={this.handleClick}>Flip</button>
-                </div>                
+                    <p>
+                        {`Out of ${this.state.c1TotalFlips} flips, there have been ${this.state.c1HeadsCount} and 
+                        ${this.state.c1TailsCount} tails.`}
+                    </p>
+                </div>
             </section>
         );
     }
 }
- 
+
 export default CoinFlip;
